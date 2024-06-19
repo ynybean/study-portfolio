c2083a229ec490f4c5b025464877d6fc4be3f17d
yewon <yewon@backpac.kr>
2024-06-19 16:46:20 +0900

    [실습] 인터셉터 개발

--- src/main/kotlin/com/myybean/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/myybean/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,8 +1,25 @@
 package com.myybean.portfolio.presentation.interceptor
 
+import com.myybean.portfolio.domain.entity.HttpInterface
+import com.myybean.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
 
+    override fun afterCompletion(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        handler: Any,
+        ex: Exception?
+    ) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
